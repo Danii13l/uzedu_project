80d03681614bc8d9599470d72ccf69df14f4cd65
aaalimdzhanov <aaalimdzhanov@outlook.com>
2022-11-18 10:32:57 +0500

    merge

--- src/db/queries/menu.ts
+++ src/db/queries/menu.ts
@@ -1,2 +1,2 @@
 export const getMenu = "SELECT id, name FROM menu";
-export const getSubMenu = "SELECT id, name, menu_id as menuId, is_gallery as typeOfForm FROM sub_menu";
+export const getSubMenu = "SELECT id, name, menu_id as menuId, is_gallery as typeOfForm, page_link as pageLink FROM sub_menu";
