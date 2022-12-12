09292c11897907b07cd04d99bef69720b0f3fb75
aaalimdzhanov <aaalimdzhanov@outlook.com>
2022-12-12 11:00:36 +0500

    add orderby

--- src/db/queries/information.ts
+++ src/db/queries/information.ts
@@ -3,13 +3,13 @@ export const addInformationQuery =
 export const editInformationQuery =
   "UPDATE information SET title = ?, title_ru = ?, title_uz = ?, description = ?, description_ru = ?, description_uz = ?, url=?  WHERE id = ?";
 export const ruInformationQuery =
-  "SELECT id,type, title_ru as title, description_ru as description, url, created_at as createdAt FROM information WHERE type=? LIMIT ? OFFSET ? ";
+  "SELECT id,type, title_ru as title, description_ru as description, url, created_at as createdAt FROM information WHERE type=?  ORDER BY created_at DESC LIMIT ? OFFSET ?";
 export const uzInformationQuery =
-  "SELECT id,type, title_uz as title, description_uz as description, url, created_at as createdAt FROM information WHERE type=?  LIMIT ? OFFSET ? ";
+  "SELECT id,type, title_uz as title, description_uz as description, url, created_at as createdAt FROM information WHERE type=?  ORDER BY created_at DESC  LIMIT ? OFFSET ? ";
 export const fullInformationQuery =
-  "SELECT id,type, title, title_ru as titleRu, title_uz as titleUz, description, description_ru as descriptionRu, description_uz as descriptionUz, url, created_at as createdAt FROM information WHERE type=?  LIMIT ? OFFSET ? ";
+  "SELECT id,type, title, title_ru as titleRu, title_uz as titleUz, description, description_ru as descriptionRu, description_uz as descriptionUz, url, created_at as createdAt FROM  ORDER BY created_at DESC information WHERE type=?  LIMIT ? OFFSET ? ";
 export const enInformationQuery =
-  "SELECT id,type, title, description, url, created_at as createdAt FROM information WHERE type=?  LIMIT ? OFFSET ? ";
+  "SELECT id,type, title, description, url, created_at as createdAt FROM information WHERE type=?  ORDER BY created_at DESC  LIMIT ? OFFSET ? ";
 export const deleteInformationById = "DELETE FROM information WHERE id=?";
 export const getInformationById =
   "SELECT id,type, title, title_ru as titleRu, title_uz as titleUz, description, description_ru as descriptionRu, description_uz as descriptionUz, url, created_at as createdAt  FROM information WHERE id=?";
